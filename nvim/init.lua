299349c55ddd6cac81a938ffa88e6b08ae465be4
Chris Dawson <chrisdawson1996@gmail.com>
2023-09-17 19:17:48 +0100

    tidy up

--- nvim/init.lua
+++ nvim/init.lua
@@ -74,6 +74,9 @@ require('lazy').setup({
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
+  -- vim tmux navigatior
+  'christoomey/vim-tmux-navigator',
+
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
